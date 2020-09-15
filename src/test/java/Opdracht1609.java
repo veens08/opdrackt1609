348bc8e2c6b76e93ceaa054b887b22e9c7c98475
veens08 <sjoerdski99@gmail.com>
2020-09-15 10:44:09 +0200

    opdracht1609

---
+++ src/test/java/Opdracht1609.java
@@ -0,0 +1,7 @@
+package org.example.sjoerd.dag03basicjava;
+
+import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+
+public class Opdracht1609 {
+}
---
+++ src/test/java/org/example/sjoerd/Opdracht1609/MijnTest.java
@@ -0,0 +1,10 @@
+package org.example.sjoerd.Opdracht1609;
+
+import org.junit.jupiter.api.Test;
+
+public class MijnTest {
+    @Test
+    void test1() {
+
+    }
+}
